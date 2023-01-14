df8e8e90b7d4483202b54f659f28fdaeab3b608b
onao13 <khovratovic15@yandex.ru>
2023-01-14 15:43:40 +0300

    Update build

--- src/main/java/onao/coffee/example/config/DBConfig.java
+++ src/main/java/onao/coffee/example/config/DBConfig.java
@@ -3,6 +3,9 @@ package onao.coffee.example.config;
 import io.ebean.DatabaseFactory;
 import io.ebean.config.DatabaseConfig;
 import io.ebean.datasource.DataSourceConfig;
+import onao.coffee.example.data.Coffees;
+import onao.coffee.example.data.CoffeesInOrder;
+import onao.coffee.example.data.Orders;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,6 +24,10 @@ public class DBConfig {
 
         DatabaseConfig config = new DatabaseConfig();
         config.setDataSourceConfig(sourceConfig);
+        config.addClass(Coffees.class);
+        config.addClass(CoffeesInOrder.class);
+        config.addClass(Orders.class);
+
         DatabaseFactory.create(config);
     }
 }
